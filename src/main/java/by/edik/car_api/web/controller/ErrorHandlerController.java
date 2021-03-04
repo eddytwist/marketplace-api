fe2ca75d786d3ea8934db1602f72548a965763c8
Eduard Ivanov <Ed_vvard@mail.ru>
2021-03-04 20:25:41 +0300

    ErrorHandlerController should be fixed.

--- src/main/java/by/edik/car_api/web/controller/ErrorHandlerController.java
+++ src/main/java/by/edik/car_api/web/controller/ErrorHandlerController.java
@@ -5,40 +5,53 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.log4j.Logger;
 
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Arrays;
 
-import static by.edik.car_api.config.ServletConstants.APPLICATION_JSON;
-import static by.edik.car_api.config.ServletConstants.UTF_8;
+import static javax.servlet.RequestDispatcher.ERROR_EXCEPTION;
 import static javax.servlet.RequestDispatcher.ERROR_EXCEPTION_TYPE;
 import static javax.servlet.RequestDispatcher.ERROR_MESSAGE;
 import static javax.servlet.RequestDispatcher.ERROR_STATUS_CODE;
 
-@WebServlet(urlPatterns = "/errorHandler")
-public class ErrorHandlerController extends HttpServlet {
+@WebServlet("/errorHandler")
+public class ErrorHandlerController extends BaseController {
 
     private final Logger log = Logger.getLogger(ErrorHandlerController.class);
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        log.info("GET method running.");
-        resp.setContentType(APPLICATION_JSON);
-        resp.setCharacterEncoding(UTF_8);
+    public void doGet(HttpServletRequest request, HttpServletResponse response) {
+        log.info("ERROR SERVICE method running.");
+        log.warn("SERVICE got exception: " + request.getAttribute(ERROR_EXCEPTION));
+
+        setResponseConfiguration(response);
+
         ObjectMapper mapper = new ObjectMapper();
-        try (PrintWriter writer = resp.getWriter()) {
+        try (PrintWriter writer = response.getWriter()) {
             Arrays.asList(ERROR_STATUS_CODE, ERROR_EXCEPTION_TYPE)
                 .forEach(e ->
-                    writer.write(e + ": " + req.getAttribute(e) + '\n')
+                    writer.write(e + ": " + request.getAttribute(e) + "\n")
                 );
             ApiErrorResponse apiErrorResponse = new ApiErrorResponse();
-            apiErrorResponse.setErrorMessage((String) req.getAttribute(ERROR_MESSAGE));
+            apiErrorResponse.setErrorMessage(request.getAttribute(ERROR_MESSAGE).toString());
             String json = mapper.writeValueAsString(apiErrorResponse);
             writer.write(json);
-            log.info("Data returned to the client: " + json);
+            log.info("Data returned to the client:\n" + json);
+
+//        executeWithResult(response, () -> {
+//            log.info("start lambda");
+//            ApiErrorResponse apiErrorResponse = ApiErrorResponse.builder()
+//                .errorMessage(request.getAttribute(ERROR_MESSAGE).toString())
+//                .errorStatusCode((Integer) (request.getAttribute(ERROR_STATUS_CODE)))
+//                .errorExceptionType(request.getAttribute(ERROR_EXCEPTION_TYPE).toString())
+//                .build();
+//            log.info("end lambda");
+//            return apiErrorResponse;
+//        });
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
